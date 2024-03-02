74f5a463b90a46718941b83f11c9c80893a0ff83
yesseruser <yesseruseryt@gmail.com>
2024-03-02 13:26:53 +0100

    Add User class. WiP

---
+++ Bakalari.NET/User.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Bakalari.NET
+{
+    public class User : ErrorInfo
+    {
+        [JsonProperty("UserUID")] public string UserID { get; set; }
+        [JsonProperty("CampaignCategoryCode")] public string CampaignCategoryCode { get; set; }
+    }
+}
